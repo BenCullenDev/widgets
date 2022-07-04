b70a32bdd1e885c44a3ddc5995ea01dbaa867b26
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-04 08:54:54 +0100

    wired up a search input

---
+++ src/components/Search.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Search = () => {
+    const [searchTerm, setSearchTerm] = useState('')
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                    className="input"
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
