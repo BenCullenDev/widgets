e9a08667d667288c08d42232669b8cfe096bbe84
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-04 11:49:29 +0100

    added render for list of results and button with link

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-    const [searchTerm, setSearchTerm] = useState('')
+    const [searchTerm, setSearchTerm] = useState('Programming')
+    const [results, setResults] = useState([])
+    
+    console.log(results)
+
+    useEffect(()=> {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: searchTerm,
+                }
+            });
+
+            setResults(data.query.search)
+        }
+
+        search()
+    }, [searchTerm])
+
+    const renderedResults = results.map((result) => {
+
+            const regex = /(<([^>]+)>)/gi;  //NEW
+            const cleanSnippet = result.snippet.replace(regex, ""); //NEW 
+
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    {cleanSnippet}
+                </div>
+            </div>
+        )
+    })
+
+
     return (
         <div>
             <div className="ui form">
@@ -14,6 +56,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     )
 }
