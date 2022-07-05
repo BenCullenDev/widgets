3f44814d8275f91bee865c9b39904070e74b314c
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-05 09:29:59 +0100

    added debouncing to stop spamming api on input

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,65 +2,75 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-    const [searchTerm, setSearchTerm] = useState('Programming')
-    const [results, setResults] = useState([])
-    
-    console.log(results)
+	const [searchTerm, setSearchTerm] = useState("Programming");
+	const [results, setResults] = useState([]);
 
-    useEffect(()=> {
-        const search = async () => {
-            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: searchTerm,
-                }
-            });
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+				params: {
+					action: "query",
+					list: "search",
+					origin: "*",
+					format: "json",
+					srsearch: searchTerm,
+				},
+			});
 
-            setResults(data.query.search)
-        }
-        if (searchTerm) {
-            search()
-        }
-        
-    }, [searchTerm])
+			setResults(data.query.search);
+		};
 
-    const renderedResults = results.map((result) => {
+		if (searchTerm && !results.length) {
+			search();
+		} else {
+			const timeoutId = setTimeout(() => {
+				if (searchTerm) {
+					search();
+				}
+			}, 500);
 
-            const regex = /(<([^>]+)>)/gi;  //NEW
-            const cleanSnippet = result.snippet.replace(regex, ""); //NEW 
+			return () => {
+				clearTimeout(timeoutId);
+			};
+		}
+	}, [searchTerm]);
 
-        return (
-            <div key={result.pageid} className="item">
-                <div className="right floated content">
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
-                </div>
-                <div className="content">
-                    <div className="header">{result.title}</div>
-                    {cleanSnippet}
-                </div>
-            </div>
-        )
-    })
+	const renderedResults = results.map((result) => {
+		const regex = /(<([^>]+)>)/gi; //NEW
+		const cleanSnippet = result.snippet.replace(regex, ""); //NEW
 
+		return (
+			<div key={result.pageid} className='item'>
+				<div className='right floated content'>
+					<a
+						className='ui button'
+						href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+						Go
+					</a>
+				</div>
+				<div className='content'>
+					<div className='header'>{result.title}</div>
+					{cleanSnippet}
+				</div>
+			</div>
+		);
+	});
 
-    return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input 
-                    className="input"
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}
-                    />
-                </div>
-            </div>
-            <div className="ui celled list">{renderedResults}</div>
-        </div>
-    )
-}
+	return (
+		<div>
+			<div className='ui form'>
+				<div className='field'>
+					<label>Enter Search Term</label>
+					<input
+						className='input'
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className='ui celled list'>{renderedResults}</div>
+		</div>
+	);
+};
 
-export default Search
+export default Search;
