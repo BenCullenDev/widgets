57d4ede688c6ff58a14035602d25c86cd2d3bccd
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-05 15:05:41 +0100

    added ability to close dropdown by clicking outside of element and added a toggle to hide completely

--- src/App.js
+++ src/App.js
@@ -35,14 +35,21 @@ const options = [
 
 const App = () => {
 	const [selected, setSelected] = useState(options[0]);
+	const [showDropdown, setShowDropdown] = useState(true);
 	return (
 		<div>
 			<h1>Widgets App</h1>
-			<Dropdown
-				options={options}
-				selected={selected}
-				onSelectedChange={setSelected}
-			/>
+			<button onClick={() => setShowDropdown(!showDropdown)}>
+				Toggle Dropdown
+			</button>
+
+			{showDropdown ? (
+				<Dropdown
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+				/>
+			) : null}
 		</div>
 	);
 };
