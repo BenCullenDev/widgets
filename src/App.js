aae755874394dde6097915fbc2e77ad6475502e3
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-06 09:45:17 +0100

    added basic routing for the 4 widget components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-// import Accordion from "./components/Accordion";
-// import Search from "./components/Search";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 
@@ -19,43 +19,53 @@ const options = [
 	},
 ];
 
-// const items = [
-// 	{
-// 		title: "What is React?",
-// 		content: "React is a frontend javascript framework.",
-// 	},
-// 	{
-// 		title: "Some other title/question",
-// 		content: "Here is some more conent.",
-// 	},
-// 	{
-// 		title: "The third title or question",
-// 		content: "Here is even more content",
-// 	},
-// ];
+const items = [
+	{
+		title: "What is React?",
+		content: "React is a frontend javascript framework.",
+	},
+	{
+		title: "Some other title/question",
+		content: "Here is some more conent.",
+	},
+	{
+		title: "The third title or question",
+		content: "Here is even more content",
+	},
+];
+
+const showAccordion = () => {
+	if (window.location.pathname === "/") {
+		return <Accordion items={items} />;
+	}
+};
+
+const showList = () => {
+	if (window.location.pathname === "/list") {
+		return <Search />;
+	}
+};
+
+const showDropdown = () => {
+	if (window.location.pathname === "/dropdown") {
+		return <Dropdown />;
+	}
+};
+
+const showTranslate = () => {
+	if (window.location.pathname === "/translate") {
+		return <Translate />;
+	}
+};
 
 const App = () => {
-	const [selected, setSelected] = useState(options[0]);
-	const [showDropdown, setShowDropdown] = useState(true);
 	return (
 		<div>
 			<h1>Widgets App</h1>
-
-			{showDropdown ? (
-				<Dropdown
-					label='Choose a Colour'
-					options={options}
-					selected={selected}
-					onSelectedChange={setSelected}
-				/>
-			) : null}
-			<button onClick={() => setShowDropdown(!showDropdown)}>
-				Toggle Colour Dropdown
-			</button>
-			<br />
-			<br />
-			<br />
-			<Translate />
+			{showAccordion()}
+			{showList()}
+			{showDropdown()}
+			{showTranslate()}
 		</div>
 	);
 };
